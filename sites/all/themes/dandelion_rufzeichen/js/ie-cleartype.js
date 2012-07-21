55695c08607162a6c28046b2c22f21d20710bb02
Thomas Moseler <tmoseler@gmx.de>
2012-07-21 18:21:51 +0200

    Advanced Options Plugin for Views Slideshow

---
+++ sites/all/themes/dandelion_rufzeichen/js/ie-cleartype.js
@@ -0,0 +1,7 @@
+(function ($) {
+  
+  $(document).ready(function(){
+    $('.view-front-header-slider').fadeIn(500, function(){
+    if ($.browser.msie){this.style.removeAttribute('filter');}});  
+
+   }); //document.ready
