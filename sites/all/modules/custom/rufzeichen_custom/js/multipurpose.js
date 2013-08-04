271a00526fd1e370f19153c79c9c6a3c63786f13
Thomas Moseler <tmoseler@gmx.de>
2013-08-04 22:39:09 +0200

    Unnötige Zeilenumbrüche entfernt

--- sites/all/modules/custom/rufzeichen_custom/js/multipurpose.js
+++ sites/all/modules/custom/rufzeichen_custom/js/multipurpose.js
@@ -11,11 +11,9 @@
         $(".colorbox").click(function(e){
           e.preventDefault();
         });
-        $(".overview-thumb-wrapper").removeAttr("bt-xtitle");
-        
+        $(".overview-thumb-wrapper").removeAttr("bt-xtitle");        
       }
     }
-
   }); //document.ready
 
 })(jQuery); 
