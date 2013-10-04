c339e04ab942c90cd46704201c3889707a18fa7f
Thomas Moseler <tmoseler@gmx.de>
2013-10-04 12:35:41 +0200

    Cron Script, um Cron auf Hosteurope ansteuern zu können

---
+++ sites/all/modules/custom/cron-lynx.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+#/usr/bin/lynx -source http://example.com/cron.php > /dev/null 2>&1
+
+lynx -dump http://rufzeichen-online.de/cron.php?cron_key=kpg4t8SihRsNul0JzxYRmpLL8Dg6P2-SlhCxvi-dPw8 > /dev/null
