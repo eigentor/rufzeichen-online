ad9f3fa688df47082eb66c0b514053aa99110191
Thomas Moseler <tmoseler@gmx.de>
2015-05-07 12:36:01 +0200

    Testcode für Google Analytics Event Tracking

--- sites/all/themes/dandelion_rufzeichen/js/rufzeichen-custom.js
+++ sites/all/themes/dandelion_rufzeichen/js/rufzeichen-custom.js
@@ -1,18 +1,47 @@
 (function ($) {
-  
-  $(document).ready(function(){
 
-	// Toggle the mobile menu
-    $('.mobile-menu-button a').toggle(function(){
-      $('#main-menu-mobile').slideDown('slow');
-    },
-    function(){
-      $('#main-menu-mobile').slideUp('slow');
-    });
+// Scripte als Drupal Behaviors hinzufuegen statt als document.ready.
+// Funktioniert dann auch bei Ajax Reloads.
+  Drupal.behaviors.oebv = {
+    attach: function (context, settings) {
+
+// Toggle the mobile menu
+$('.mobile-menu-button a').toggle(function(){
+  $('#main-menu-mobile').slideDown('slow');
+},
+function(){
+  $('#main-menu-mobile').slideUp('slow');
+});
+
+/**
+ * Wrapper function for Google Analytics events
+ * From: https://www.commercialprogression.com/post/google-analytics-events-drupal
+ */
+
+//function ga_event(params) {
+//  params.splice(0, 0, "_trackEvent");
+//  if (typeof _gaq === "object") {
+//    _gaq.push(params);
+//  }
+//};
+//
+//// Capture Next and Prev events in Slideshow
+//$(".front .flex-direction-nav a").bind('click', function(e){
+//  if ($(this).hasClass("flex-prev")) {
+//    ga_event(['FP Slideshow', 'Arrow Right']);
+//    _gaq.push(['_trackEvent', 'Slider', 'Navigation', 'Slide back']);
+//  }
+//  else {
+//    ga_event(['FP Slideshow', 'Arrow Left']);
+//  }
+//});
+
+$('#front-main-text h2').click(function(){
+  _gaq.push(['_trackEvent', 'Inhalt', 'Ueberschrift', 'Seitenueberschrift angeklickt']);
+});
 
 
-   // end document_ready()
-  });
+    } // end of attach function
+  };
 
-// end wrapper function
 })(jQuery);
