26da4d4fb4f5f9e4d02ea254aeaa4e4dd95691c4
Thomas Moseler <tmoseler@gmx.de>
2017-09-05 13:36:06 +0200

    Höhe Slideshow Main Frame auf Projektseite automatisch anpassen.

--- sites/all/themes/dandelion_rufzeichen/js/rufzeichen-custom.js
+++ sites/all/themes/dandelion_rufzeichen/js/rufzeichen-custom.js
@@ -1,8 +1,5 @@
 (function ($) {
-
-// Scripte als Drupal Behaviors hinzufuegen statt als document.ready.
-// Funktioniert dann auch bei Ajax Reloads.
-  Drupal.behaviors.oebv = {
+  Drupal.behaviors.rufzeichen_theme = {
     attach: function (context, settings) {
 
 // Toggle the mobile menu
@@ -24,23 +21,24 @@ function ga_event(params) {
     _gaq.push(params);
   }
 };
-//
-// Capture Next and Prev events in Slideshow
-//$(".front .flex-direction-nav a").bind('click', function(e){
-//  if ($(this).hasClass("flex-prev")) {
-//    ga_event(['FP Slideshow', 'Arrow Left']);
-//    _gaq.push(['_trackEvent', 'Slider', 'Navigation', 'Slide back']);
-//  }
-//  else {
-//    ga_event(['FP Slideshow', 'Arrow Right']);
-//  }
-//});
-
 
 $('#front-main-text h2').click(function(){
   //_gaq.push(['_trackEvent', 'Inhalt', 'Ueberschrift', 'Klick mal was']);
   ga_event(['Ueberschrift', 'Hover']);
 });
+
+function resize_slideshow() {
+  var slideshow_img_height = $('.node-type-projekt .view-images-project .views-slideshow-cycle-main-frame .field-content img').height() + 10;
+  $('.node-type-projekt .view-images-project .views-slideshow-cycle-main-frame').height(slideshow_img_height);
+  var slideshow_img_width = $('.node-type-projekt .view-images-project .views-slideshow-cycle-main-frame .field-content img').width();
+  $('.node-type-projekt .view-images-project .views-slideshow-cycle-main-frame').width(slideshow_img_width);
+}
+
+$(window).resize(function(){
+   resize_slideshow();
+})
+
+
 
 
     } // end of attach function
